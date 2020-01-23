f83ef038299832bb4dd9f68a3ff0da465d16d4d3
Matt Shepardson <lupemls@yahoo.com>
2020-01-23 13:26:13 -0800

    added an event listener for when a user clicks on one of the video game images, searching the youTube API for videos of the game and appending 5 videos to the top of the page

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+
+
 $(document).ready(function(){
     
+    //upon searching, this will display the results by printing the name followed by a thumbnail of the game, currently adding newer searches above the previous ones
     $('#search').on('submit', function(event){
         event.preventDefault();
         let p = $('p');
@@ -30,9 +33,27 @@ $(document).ready(function(){
     
     })
 
+    //this will allow a user to click one of the images, searching the youtube API, and will embed 5 videos to the top of the page
+    //user might get ERR_BLOCKED_BY_CLIENT if they have an adblocker
     $('body').on('click','img', function(){
         console.log($(this).attr('alt'));
         let game = $(this).attr('alt');
+        let queryUrl = `https://www.googleapis.com/youtube/v3/search?key=AIzaSyAnOFnUw0G81BnJj2mpFBAbBe8QStxPUzs&part=snippet&q=${game}`;
+        $.ajax({
+            url: queryUrl,
+            method:'GET'
+        }).then(function(response) {
+            console.log(response);
+            let vid = $('#video');
+            vid.empty();
+            for(let i = 0; i < response.items.length; i++){
+                let videoUrl = `https://www.youtube.com/embed/${response.items[i].id.videoId}`;
+                let video = $('<iframe>');
+                video.attr('src', videoUrl);
+                vid.append(video);
+            }
+
+        })
     })
 
 })
