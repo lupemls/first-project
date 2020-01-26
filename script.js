e8a82b5043a87ad2f3113e26c72199d073325cda
Matt Shepardson <lupemls@yahoo.com>
2020-01-25 22:11:14 -0800

    Added a feature that added more screenshots on click, added a condition where searching without inputting any value will no longer work, and limitted the number of screenshots to be the number provided by the API up to a max of 5

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
+// Updated site for atari 2600 search
 $(document).ready(function () {
+    let listOfResults;
+
     //This will search for the game name and a platfrom that the user submits, and append results to the page
     $('#search').on('submit', function (e) {
         e.preventDefault();
-        let platform = platformSearcher();
-
         let game = $('#game').val();
-        let queryUrl = 'https://rawg.io/api/games';
-        if(game == null){
+
+        //if there is no input it will not search
+        if(game == ""){
             return;
-        }else if(platform == null){
-            queryUrl = `${queryUrl}?search=${game}`
-        }else{
-            queryUrl = `https://rawg.io/api/games?search=${game}&parent_platforms=${platform}`;
         }
-        // console.log(queryUrl);
+        let queryUrl = `https://rawg.io/api/games?search=${game}&platforms=23`;
 
         $.ajax({
             url: queryUrl,
             method: 'GET'
         }).then(function(response){
             printGames(response);
+            listOfResults = response.results;
         })
     })
 
@@ -28,84 +27,74 @@ $(document).ready(function () {
     $('body').on('click','.image', function(){
         console.log($(this).attr('alt'));
         let game = $(this).attr('alt');
-        let queryUrl = `https://www.googleapis.com/youtube/v3/search?key=AIzaSyAnOFnUw0G81BnJj2mpFBAbBe8QStxPUzs&part=snippet&q=${game}`;
+        let queryUrl = `https://www.googleapis.com/youtube/v3/search?key=AIzaSyCBZbc5OhnbOKL7oKitbYrGa0F6_P5kLQU&part=snippet&q=${game}+atari+2600`;
+        
         $.ajax({
             url: queryUrl,
             method:'GET'
         }).then(function(response) {
-            // console.log(response);
-            videoSearcher(response);
+            printMoreInfo(game, response);
         })
     })
 
-    //this will append the 5 youtube video results of the game to the page
-    function videoSearcher(response){
-            let vid = $('#video');
-            vid.empty();
-            for(let i = 0; i < response.items.length; i++){
-                let videoUrl = `https://www.youtube.com/embed/${response.items[i].id.videoId}`;
-                let video = $('<iframe>');
-                video.attr('src', videoUrl);
-                vid.append(video);
+    //this will append the additional images and the 5 youtube video results of the game to the page
+    function printMoreInfo(game, response){
+        let info = $('#info');
+        //clears previous click's results
+        info.empty();
+        let picDiv = $('<div>');
+        picDiv.attr('class', 'screenshots')
+        let picArray;
+        // adds additional screenshots of the game
+        for(let i = 0; i < listOfResults.length; i++){
+            console.log(listOfResults[0].name, game)
+            if(listOfResults[i].name == game){
+                picArray = listOfResults[i].short_screenshots;                
             }
+        }
+        let num = 6;
+        //sets the number of images to fit the number in the array or keeps the max at 5
+        if(picArray.length < num){
+            num = picArray.length - 1;
+        }
+
+        //appends the 5 or less images after the thumbnail image(index 0)
+        for(let j = 1; j < num; j++){
+            let screenshot = $('<img>');
+            screenshot.attr({
+                'src': picArray[j].image,
+                'class': 'screenshot'
+            });
+            picDiv.append(screenshot);
+        }
+        info.append(picDiv);
+        //appends the 5 videos to the page
+        for(let i = 0; i < response.items.length; i++){
+            let videoUrl = `https://www.youtube.com/embed/${response.items[i].id.videoId}`;
+            let video = $('<iframe allowFullScreen>');
+            video.attr('src', videoUrl);
+            info.append(video);
+        }
     }
 
     //This will append the game name and the image to the page
     function printGames(response){
-            console.log(response);
-            let p = $('p');
-            p.empty();
-            $('#video').empty();
-            let array = response.results;
-        
-            for(let i = 0; i<array.length; i++){
-                // console.log(array[i]);
-                let name = $('<div>').text(array[i].name)
-                let image = $('<img>').attr({
-                    'class': 'image',
-                    'src': array[i].background_image,
-                    'alt': array[i].name
-                });
-                p.append(name, image);
-            }
-    }
-
-    //This allows the user to search the RAWG API by platform as well as name
-    function platformSearcher(){
-        let platform;
-        let value = $('#platform').val().toLowerCase();
-        let split = value.split(' ');
-        let keys = Object.keys(platforms);
-        for (let i = 0; i < split.length; i++) {
-
-            if (keys.includes(split[i].toLowerCase())) {
-                if (platform == null) {
-                    platform = platforms[split[i]];
-                } else {
-                    platform = `${platform},${platforms[split[i]]}`;
-                }
-            }
+        console.log(response);
+        let p = $('p');
+        p.empty();
+        $('#info').empty();
+        let array = response.results;
+    
+        for(let i = 0; i<array.length; i++){
+            let name = $('<div>').text(array[i].name)
+            let image = $('<img>').attr({
+                'class': 'image',
+                'src': array[i].background_image,
+                'alt': array[i].name
+            });
+            p.append(name, image);
         }
-        // console.log('platforms', platform);
-        return platform;
     }
 
 })
 
-//RAWG parent platforms are coded so that they correspond to numbers, so this is a list of the different parent platforms that is not easily attainable through the API
-let platforms = {
-    pc: 1,
-    playstation: 2,
-    xbox: 3,
-    ios: 4,
-    // 'apple macintosh': 5,
-    linux: 6,
-    nintendo: 7,
-    android: 8,
-    atari: 9,
-    // 'commodore/amiga': 10,
-    sega: 11,
-    // '3d0': 12,
-    // 'neo geo': 13,
-    web: 14
-}
